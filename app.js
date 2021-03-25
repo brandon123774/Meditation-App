2d99b3b40623c57be748ae19f1db475a0ace357c
Brandon Shankar <bshankar12@comcast.net>
2021-03-24 21:45:28 -0700

    added more styling and started js

---
+++ app.js
@@ -0,0 +1,6 @@
+var app = () => {
+    var song = document.querySelector('.song');
+    var play = document.querySelector('.play');
+    var outline = document.querySelector('.moving-outline circle');
+
+}
