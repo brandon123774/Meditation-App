cfd5875943cdea95c59cf3ff5ebf42424e6c64cf
Brandon Shankar <bshankar12@comcast.net>
2021-03-28 22:00:14 -0700

    added countdown timer and sound/video start/pause/reset

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = () => {
 
     //Time display
     var timeDisplay = document.querySelector('.time-display');
+    var timeSelect = document.querySelectorAll('.time-select button')
 
     //length of the outline 
     var outlineLength = outline.getTotalLength();
@@ -20,9 +21,26 @@ var app = () => {
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
+    //Pick different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function()  {
+            song.src = this.getAttribute('data=sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        })
+    })
+
     //Play Sound
     play.addEventListener('click', () =>    {
         checkPlaying(song);
+    });
+
+    //Select Sound
+    timeSelect.forEach(option   =>  {
+        option.addEventListener('click', function() {
+            fakeDuration = this.getAttribute("data-time");
+            timeDisplay.textContent = '${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}';
+        });
     });
 
     //Create a function to stop and play the sounds
@@ -47,9 +65,18 @@ var app = () => {
         //Progress bar animation
         var progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
         outline.style.strokeDashoffset = progress;
-        
 
-    }
+        // Animate the text
+        timeDisplay.textContent = '${minutes}:${seconds}';
+
+        if(currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = "./svg/play.svg";
+            video.pause();
+        }
+
+    };
 };
 
 app();
